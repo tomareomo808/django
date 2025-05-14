e21ce6d4bd820e13b9dbf627820068ad79cbcc65
lanzolinedu <luca.anzolin@isisluzzatto.onmicrosoft.com>
2025-05-14 10:47:19 +0200

    Update views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+     context = {
+        'messaggio': 'Benvenuto nella homepage!'
+    }
+
+    return render(request, 'home.html', context)
